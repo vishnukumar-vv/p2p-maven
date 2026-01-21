2738b9005268c0e1358913ca3ad324eb87817a62
vishnukumar-vv <vishnu_kumar2@zohomail.in>
2026-01-21 11:13:49 +0530

    Update App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class app {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven Sample App!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
